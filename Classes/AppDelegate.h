ccc1b3d408d98b8bc293205fb032138e00d8676a
aaslun <andreas.lundgren@norran.se>
2012-04-26 12:55:41 +0200

    Added project

---
+++ Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  revolver_tabs
+//
+//  Created by Andreas Lundgren on 2012-04-26.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit revolver_tabs.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  revolver_tabs
+//
+//  Created by Andreas Lundgren on 2012-04-26.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
